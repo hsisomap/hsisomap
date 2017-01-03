76a47538529d871da0b29e142bde93f960b19933
C. Jin <cjin@users.noreply.github.com>
2017-01-02 21:27:32 -0800

    update travis

--- generateDocumentationAndDeploy.sh
+++ generateDocumentationAndDeploy.sh
@@ -50,7 +50,7 @@ echo '-'
 
 ##### Configure git.
 # Set the push default to simple i.e. push only the current branch.
-git config --global push.default simple
+git config push.default simple
 echo '0'
 # Pretend to be an user called Travis CI.
 git config user.name "Travis CI"
